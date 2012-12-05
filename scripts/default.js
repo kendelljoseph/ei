48d5a5bbbc027a8a70da260ba44b81b9f3c2c9e0
Kendell Joseph <contact@kendelljoseph.com>
2012-12-05 11:10:39 -0500

    Almost to a point where I am not overhauling the whole thing

--- scripts/default.js
+++ scripts/default.js
@@ -1,3 +1,8 @@
+
+/*  Eilers Innovations
+*   Eventually I'll comment this thing...
+*/
+
 $(function(){
     var body =  $('body');
     var title = $('<div />')
@@ -21,6 +26,10 @@ $(function(){
                 .attr('id', 'ei_reviews')
                 .attr('class','ei_menuItem')
                 .html('Reviews');
+    var onTheTable = $('<div />')
+                .attr('id', 'ei_onTheTale')
+                .attr('class','ei_menuItem')
+                .html('On the Table');
     var pedalMods = $('<div />')
                 .attr('id', 'ei_pedalMods')
                 .attr('class','ei_menuItem')
@@ -29,13 +38,19 @@ $(function(){
                 .attr('id', 'ei_ampMods')
                 .attr('class','ei_menuItem')
                 .html('Amp Modifications');
-    var video = $('<div />')
-                .attr('id', 'ei_pedalVideo');
+    var socialMedia = $('<div />')
+                .attr('id', 'ei_socialMedia');
+    var facebookImage = $('<img />')
+                .attr('id', 'ei_fbImage')
+                .attr("src", 'images/facebook.png');
+    var twitterImage = $('<img />')
+                .attr('id', 'ei_twtrImage')
+                .attr("src", 'images/twitter.png');
+                
     var image = $('<img />')
-                .attr('id', 'ei_pedalImage');
+                .attr('id', 'ei_ytImage');
     var description = $('<div />')
-                .attr('id', 'ei_description')
-                .html('This is a pedal description 1 2 3 4 5 6 7 8 9 0, those were numbers, this is long'); 
+                .attr('id', 'ei_ytDescription');
     
     function centerElement(ele){
         ele.css({
@@ -47,6 +62,27 @@ $(function(){
         centerElement(title);
         centerElement(menu);
         centerElement(home);
+        setInterval(function(){
+            twitterImage.rotate({
+                duration: 8000,
+                angle: 0,
+                animateTo: 360
+            });
+            facebookImage.rotate({
+                duration: 10000,
+                angle: 0,
+                animateTo: -360
+            });   
+        },15000);
+        
+    }
+    
+    function reset(ele, defaultPos){
+        ele.animate(defaultPos);
+        if(ele.css('transform') !== "matrix(1, 0, 0, 1, 0, 0)"){
+            image.rotate({angle: -30, animateTo: 0});
+            description.rotate({angle:10, animateTo: 0});
+        }
     }
     
     function setPosition(pos){
@@ -61,55 +97,32 @@ $(function(){
         }
     }
     
+    socialMedia.append(facebookImage)
+        .append(twitterImage);
     menu.append(home)
         .append(about)
         .append(contact)
         .append(reviews)
+        .append(onTheTable)
         .append(pedalMods)
         .append(ampMods);
     body.append(title)
         .append(menu)
-        .append(video)
         .append(image)
-        .append(description);
+        .append(description)
+        .append(socialMedia);
     
     arrangeElements();
     
-    image.click(function(){
-        var topPos = parseInt(image.css("top"),10) - 30;
-        var rightPos = parseInt(image.css("right"),10) + 30;
-        if(topPos < -120){
-            topPos = -30;
-            rightPos = 320;
-            
-            image.animate({
-                top: topPos,
-                right: rightPos
-            });
-        } else {
-            image.animate({
-                top: topPos,
-                right: rightPos
-            });
-        }
-    });
-    
-    function reset(ele, defaultPos){
-        ele.animate(defaultPos);
-    }
-    
-    image.dblclick(function(){
-        reset(image,{
-            top: -30,
-            right: 320
-        });
-    });
-    
     function eiVideoPlayer(options){
-        title.html(options.caller.html());  // Title becomes HTML of the element that was clicked.
-        description.html(options.descripntion);
+        $('#ei_ytVideo').remove();
+        body.append($('<div />').attr('id','ei_ytVideo'));
         
-        video.css('display','block');
+        title.html(options.caller.html());  // Title becomes HTML of the element that was clicked.
+        description.html(options.description);
+        image.attr('src', options.image);
+        
+        $('#ei_ytVideo').css('display','block');
         image.css('display','block');
         
         description.css('display','block');
@@ -118,82 +131,85 @@ $(function(){
             right: 320
         });
         reset(description,{
-            top: 350,
-            right: 20
+            top: 250,
+            right: 50
         });
         
-        function onPlayerReady(){
-                // Do something? not sure yet
+        function onPlayerReady(e){
         }
         
         function onPlayerStateChange(e){
             var state = e.data;
-            var ele = e.target;
             
             if(state == 1 || state == 3){
                 image.animate({
-                    top: -110,
-                    right: 400
+                    top: -40,
+                    right: 420
                 });
                 description.animate({
                     top: 400,
                 });
-                description.rotate({
-                    angle:10,
-                    animateTo: 50
-                })
+                image.rotate({angle:0, animateTo: -30});
+                description.rotate({angle:0, animateTo: 10});
             }
-            if(state == 2){
+            else if(state == 2){
                 reset(image,{
                     top: -30,
                     right: 320
                 });
                 reset(description,{
-                    top: 350,
-                    right: 20
+                    top: 250,
+                    right: 50
                 });
+                image.rotate({angle: -30, animateTo: 0});
+                description.rotate({angle:10, animateTo: 0});
             }
-            console.log(state, ele);
         }
-        var player = new YT.Player('ei_pedalVideo', {
-            videoId: 'Lf7frCzDBS8',
+        
+        var player = new YT.Player('ei_ytVideo', {
+            videoId: options.videoId,
             events: {
               'onReady': onPlayerReady,
               'onStateChange': onPlayerStateChange
             }
         });
-        
-        image.attr('src','images/labels/bluejay.png');
     }
+    
     menu.click(function(){
         setPosition(1);
     });
     
-    pedalMods.click(function(){
-        var descriptionText = "This is a pedal mod! This is example text, and this text has run on too far."
+    
+    onTheTable.click(function(){
+        var descriptionText = "Boss DD-3 EI Mod currrently on the table by Eilers Innovations";
         var options = {
             caller: $(this),
+            videoId: '6JBvclCDynk',
+            image: 'images/labels/bossdd3.png',
+            description: descriptionText
+        };
+        eiVideoPlayer(options);
+    });
+    
+    pedalMods.click(function(){
+        var descriptionText = "The \"Blue Jay\" is an Eilers Innovations original I made last year for John Doe(?) of Sweet Band(?) in 2012.";
+        var options = {
+            caller: $(this),
+            videoId: 'Lf7frCzDBS8',
+            image: 'images/labels/bluejay.png',
             description: descriptionText
         };
         eiVideoPlayer(options);
     });
     
     ampMods.click(function(){
-        title.html('Amp Modifications');
-        description.html('This is the description of a Carvin MTS3200 Mod done by Eilers Innovations.');
-        
-        video.css('display','block');
-        image.css('display','block');
-        description.css('display','block');
-        reset(image,{
-            top: -30,
-            right: 320
-        });
-        reset(description,{
-            top: 350,
-            right: 20
-        });
-        //video.attr('src','http://www.youtube.com/embed/HiHSNaqpfUs');
-        image.attr('src','images/labels/carvinMTS3200.png');
+        var descriptionText = 'This is a Carvin MTS3200 Mod where I changed a thing(?) and anotherThing(?) by Eilers Innovations.';
+        var options = {
+            caller: $(this),
+            videoId: 'HiHSNaqpfUs',
+            image: 'images/labels/carvinMTS3200.png',
+            description: descriptionText
+        };
+        eiVideoPlayer(options);
     });
 });
