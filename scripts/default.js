646b029a4ac3594ad2e7dfba57ee93732048b5d4
Kendell Joseph <contact@kendelljoseph.com>
2012-12-05 15:16:17 -0500

    Tons of changes again, horrible commenting

--- scripts/default.js
+++ scripts/default.js
@@ -4,28 +4,114 @@
 */
 
 $(function(){
-    var body =  $('body');
-    var title = $('<div />')
+    
+    /* 
+    *   Here we create all our page elments, most are divs, this should be
+    *   combined into one thing that creates all these things automatically.
+    */
+    var body =  $('body');                  // Document Body
+    var title = $('<div />')                // Title
                 .attr('id','ei_title')
                 .html('Eilers Innovations');
-    var menu = $('<div />')
+    var menu = $('<div />')                 // Main Menu
                 .attr('id', 'ei_menu');
-    var home = $('<div />')
-                .attr('id', 'ei_home')
-                .attr('class','ei_menuItem')
-                .html('Home');
-    var about = $('<div />')
-                .attr('id', 'ei_about')
-                .attr('class','ei_menuItem')
-                .html('About');
-    var contact = $('<div />')
-                .attr('id', 'ei_contact')
-                .attr('class','ei_menuItem')
-                .html('Contact');
-    var reviews = $('<div />')
-                .attr('id', 'ei_reviews')
-                .attr('class','ei_menuItem')
-                .html('Reviews');
+    var videoBrowser = $('<div />')
+                .attr('id', 'ei_videoBrowser');
+                
+    var inventoryReady = false;
+    var inventory = [];
+    
+    // Async build of the main menu options
+    $(document).on('ei_menuReady', function(e, id, html, weight){
+        var eleId = "ei_" + id;
+        var menuItem = $('<div />')
+                    .attr('id', eleId)
+                    .addClass('ei_menuItem')
+                    .html(html)
+                    .click(function(){
+                        $(document).trigger('ei_pageMode', id);
+                    });
+        
+        $('#ei_menu').append(menuItem); // Append to parent
+    });
+    
+    $(document).on('ei_inventoryReady', function(e, id, name, description, videoId, image, weight){
+        inventoryReady = true;
+        var row = {
+            id: id,
+            name: name,
+            description: description,
+            videoId: videoId,
+            image: image
+        };
+        inventory.push(row); // Hold inventory values as row objects
+    });
+    
+    $(document).on('ei_showInventory',function(e, id){
+        console.log(inventory);
+        switch(id) {
+            case 'pedalMods':
+                break;
+            case 'ampMods':
+                break;
+        }
+    });
+    
+    $(document).on('ei_pageMode', function(e, id){
+        var videoBrowser = $('#ei_videoBrowser');
+        $('#ei_ytVideo').remove();
+        videoBrowser.append($('<div />').attr('id','ei_ytVideo'));  // Refresh the YTVideo Box
+        
+       switch(id) {
+            case 'home':
+                title.animate({
+                    top: "100px",
+                    left: (body.width()/2 - title.width()/2)
+                });
+                menu.animate({
+                    left: (body.width()/2)
+                });
+                videoBrowser.fadeOut();    // Hide the video Browser
+                break;
+            case 'pedalMods':
+                title.animate({
+                    top: 0,
+                    left: 10
+                });
+                menu.animate({
+                    left: 10
+                });
+                videoBrowser.fadeIn();    // Show the video Browser
+                $(document).trigger('ei_showInventory', id);
+                break;
+            case 'ampMods':
+                title.animate({
+                    top: 0,
+                    left: 10
+                });
+                menu.animate({
+                    left: 10
+                });
+                videoBrowser.fadeIn();    // Show the video Browser
+                $(document).trigger('ei_showInventory');
+                break;
+            default:
+                break;
+       }
+    });
+    
+    // Runs stuff
+    function menuHandler(){
+        var descriptionText = "Boss DD-3 EI Mod currrently on the table by Eilers Innovations";
+        var options = {
+            caller: $(this),
+            videoId: '6JBvclCDynk',
+            image: 'images/labels/bossdd3.png',
+            description: descriptionText
+        };
+        eiVideoPlayer(options);
+    }
+    
     var onTheTable = $('<div />')
                 .attr('id', 'ei_onTheTale')
                 .attr('class','ei_menuItem')
@@ -52,16 +138,24 @@ $(function(){
     var description = $('<div />')
                 .attr('id', 'ei_ytDescription');
     
+    /*
+    *   Another Idea (bottom -> top) that could be combined, all seem to have
+    *   to do with the position of elements. The final idea could also be
+    *   useful to determine visibility as well.
+    */
     function centerElement(ele){
         ele.css({
            left: (body.width()/2 - ele.width()/2)
         });
     }
     
+    /* 
+    *   Here we are again (if reading from bottom to top) is another idea that
+    *   could be combined into one idea with the two below.
+    */
     function arrangeElements(){
         centerElement(title);
         centerElement(menu);
-        centerElement(home);
         setInterval(function(){
             twitterImage.rotate({
                 duration: 8000,
@@ -77,6 +171,10 @@ $(function(){
         
     }
     
+    /*  
+    *   This needs to be changed to be pulling from a preset list of default
+    *   positions, and reset all page elements to their "first" position.
+    */
     function reset(ele, defaultPos){
         ele.animate(defaultPos);
         if(ele.css('transform') !== "matrix(1, 0, 0, 1, 0, 0)"){
@@ -85,35 +183,23 @@ $(function(){
         }
     }
     
-    function setPosition(pos){
-        if(pos === 1){
-            title.animate({
-                top: 0,
-                left: 10
-            });
-            menu.animate({
-                left: 10
-            });
-        }
-    }
+    /* The alarming redundancy here means I need to make a constructor or a loop.. or both */
+    socialMedia.append(facebookImage)   // I put the facebook image in the social media box
+        .append(twitterImage);          // I put the twitter image in the social media box
+    body.append(title)          // I put the title into the body of the page
+        .append(menu)           // I put the menu box into the body of the page
+        .append(videoBrowser)
+        .append(image)          // I put the image into the body of the page
+        .append(description)    // I put the description into the body of the page
+        .append(socialMedia);   // I put the social Media box into the body of the page
     
-    socialMedia.append(facebookImage)
-        .append(twitterImage);
-    menu.append(home)
-        .append(about)
-        .append(contact)
-        .append(reviews)
-        .append(onTheTable)
-        .append(pedalMods)
-        .append(ampMods);
-    body.append(title)
-        .append(menu)
-        .append(image)
-        .append(description)
-        .append(socialMedia);
+    arrangeElements();  // These types of calls should be run based on a load event
     
-    arrangeElements();
     
+    /* 
+    *   This is the video player, and kind of the heart of the page. This is
+    *   Where the youtube API is used.
+    */
     function eiVideoPlayer(options){
         $('#ei_ytVideo').remove();
         body.append($('<div />').attr('id','ei_ytVideo'));
@@ -136,6 +222,7 @@ $(function(){
         });
         
         function onPlayerReady(e){
+            // Eventually this will matter
         }
         
         function onPlayerStateChange(e){
@@ -174,12 +261,7 @@ $(function(){
             }
         });
     }
-    
-    menu.click(function(){
-        setPosition(1);
-    });
-    
-    
+
     onTheTable.click(function(){
         var descriptionText = "Boss DD-3 EI Mod currrently on the table by Eilers Innovations";
         var options = {
---
+++ scripts/settings.js
@@ -0,0 +1,45 @@
+
+// EI Page Settings
+$(function(){
+    var MENU_SETTINGS_KEY = "0AkOIIIT7wBStdFlNUEZRU3ZYc1JDV1dlbEZ3cGhENUE"; 
+    var INVENTORY_KEY = "0AkOIIIT7wBStdFZJM1F1NTZ6U01jdG1DRFRCT0NtQVE";
+    
+    var GSS_MAINMENU = "https://spreadsheets.google.com/feeds/list/"+
+                MENU_SETTINGS_KEY + "/od6/public/values?alt=json";
+    var GSS_SETTINGS = "https://spreadsheets.google.com/feeds/list/"+
+                MENU_SETTINGS_KEY + "/od7/public/values?alt=json";
+    
+    var GSS_INVENTORY = "https://spreadsheets.google.com/feeds/list/"+
+                INVENTORY_KEY + "/od6/public/values?alt=json";
+    var GSS_REVIEWS = "https://spreadsheets.google.com/feeds/list/"+
+                INVENTORY_KEY + "/od7/public/values?alt=json";
+    
+    getGSS(GSS_MAINMENU,menuHandler);
+    //getGSS(GSS_SETTINGS,settingsHandler);
+    
+    getGSS(GSS_INVENTORY,inventoryHandler);
+    //getGSS(GSS_REVIEWS,reviewsHandler);
+    
+    function menuHandler(data){
+        for(var i=0; i < data.length; i++){
+            $(document).trigger('ei_menuReady', data[i]);    
+        }
+    }
+    function settingsHandler(data){
+        console.log(data);
+        for(var i=0; i < data.length; i++){
+            $(document).trigger('ei_settingsReady', data[i]);    
+        }
+    }
+    function inventoryHandler(data){
+        for(var i=0; i < data.length; i++){
+            $(document).trigger('ei_inventoryReady', data[i]);    
+        }
+    }
+    function reviewsHandler(data){
+        console.log(data);
+        for(var i=0; i < data.length; i++){
+            $(document).trigger('ei_reviewsReady', data[i]);    
+        }
+    }
+});
